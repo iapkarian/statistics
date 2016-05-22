fdb11b121d2e9cb8f6806434318e137d4ba3a376
Irina <apkarianirina@gmail.com>
2016-05-22 17:35:48 +0300

    Add new columns

--- population_growth.py
+++ population_growth.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def Reindex1(olddata):
     lst1 = []
@@ -11,19 +12,31 @@ def Reindex1(olddata):
     newdata = pd.DataFrame(olddata, columns=['Year', 'Population'])
     return newdata
 
-index = 0
-srt2 = pd.DataFrame()
+def NewIndexPop(strold):  # add new columns
+    lst1 = []
+    lst2 = []
+    elem1 = strold['Population'].iloc[0]
+    for elem2 in strold['Population']:
+        lst1.append(elem2 / elem1)
+        lst2.append(elem2 - elem1)
+        elem1 = elem2
+    lst1[0] = np.nan
+    lst2[0] = np.nan
+    strold['Growth'] = lst2
+    strold['%'] = lst1
+
 xls = pd.ExcelFile('growth.xls')
 xls = xls.parse('Sheet1')
 df = pd.DataFrame(xls)
-str1 = df.sort_index(by='Year', ascending=True)
+str1 = df.sort_index(by='Year', ascending=True)  # sort index
 
-str = Reindex1(str1)
-for elem in str['Year']:
-    if elem < 1955:
-        srt2 = str.drop(index, axis=0)
-    index += index
+str1 = Reindex1(str1)
+strdel = Reindex1(str1)
+indexstr = 0
+for elem in str1['Year']:  # delete elem < 1953
+    if elem < 1953:
+        strdel = strdel.drop(indexstr, axis=0)
+    indexstr += 1
 
-
-print(str)
-# df['new'] = #add to end
+NewIndexPop(strdel)
+print(strdel)
