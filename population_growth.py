a1dbab39d82adab55a0298b23b25f17589f787da
Irina <apkarianirina@gmail.com>
2016-05-22 20:29:52 +0300

    Graphs

--- population_growth.py
+++ population_growth.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def Reindex1(olddata):
     lst1 = []
@@ -25,6 +26,27 @@ def NewIndexPop(strold):  # add new columns
     strold['Growth'] = lst2
     strold['%'] = lst1
 
+def Graphs(graph1, var):
+    fig = plt.figure()
+    if var == 1:
+        varstr = 'Growth'
+    else:
+        varstr = '%'
+    lst1 = []
+    lst2 = []
+    lst3 = []
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 1, 2)
+    for elem in graph1['Year']:
+        lst1.append(elem)
+    for elem in graph1['Population']:
+        lst2.append(elem)
+    for elem in graph1[varstr]:
+        lst3.append(elem)
+    ax1.bar(lst1, lst2, color='green')
+    ax2.errorbar(lst1, lst3)
+    plt.show()
+
 xls = pd.ExcelFile('growth.xls')
 xls = xls.parse('Sheet1')
 df = pd.DataFrame(xls)
@@ -33,10 +55,12 @@ str1 = df.sort_index(by='Year', ascending=True)  # sort index
 str1 = Reindex1(str1)
 strdel = Reindex1(str1)
 indexstr = 0
-for elem in str1['Year']:  # delete elem < 1953
+for elem in str1['Year']:  # delete elements < 1953
     if elem < 1953:
         strdel = strdel.drop(indexstr, axis=0)
     indexstr += 1
 
 NewIndexPop(strdel)
 print(strdel)
+var = 1
+Graphs(strdel, var)
