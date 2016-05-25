726ccc2c2eba445f4a2205605fcba1fd7eee27ee
Irina <apkarianirina@gmail.com>
2016-05-25 13:46:30 +0300

    Add table

--- population_growth.py
+++ population_growth.py
@@ -1,19 +1,52 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
+import tkinter.tix
 
-def Reindex1(olddata):
+
+def main_window(w, df):
+    w.title('Population growth')
+    tab = tkinter.tix.HList(w, columns=4, header=True, width=30, height=20)
+    tab.grid(row=0, column=0, columnspan=3, )
+    tab.header_create(0, text="Year")
+    tab.header_create(1, text="Population")
+    tab.header_create(2, text="Growth")
+    tab.header_create(3, text="%")
+
+    index = 0
+    for elem in df['Year']:
+        tab.add(index, data="--<%s>--" % index)
+        tab.item_create(index, 0, text=str(elem))
+        index += 1
+
+    index = 0
+    for elem in df['Population']:
+        tab.item_create(index, 1, text=str(elem))
+        tab.item_create(index, 2, text=str(elem))
+        tab.item_create(index, 3, text=str(elem))
+        index += 1
+
+    but1 = tk.tix.ButtonBox(w)
+    but1.add('ok', text='Growth', width=5)
+    but1.bind("<Button-1>", lambda event: graphs(df))
+    but1.grid(row=0, column=4)
+
+
+def reindex1(olddata):
     lst1 = []
     lst2 = []
     for elem1 in olddata['Year']:
         lst1.append(elem1)
     for elem2 in olddata['Population']:
         lst2.append(elem2)
-    olddata = dict([('Year', lst1), ('Population', lst2)])
+    # olddata = dict([('Year', lst1), ('Population', lst2)])
+    olddata = {'Year': lst1, 'Population': lst2}
     newdata = pd.DataFrame(olddata, columns=['Year', 'Population'])
     return newdata
 
-def NewIndexPop(strold):  # add new columns
+
+def new_index_pop(strold):  # add new columns
     lst1 = []
     lst2 = []
     elem1 = strold['Population'].iloc[0]
@@ -26,12 +59,10 @@ def NewIndexPop(strold):  # add new columns
     strold['Growth'] = lst2
     strold['%'] = lst1
 
-def Graphs(graph1, var):
+
+def graphs(graph1):
+    print('fff')
     fig = plt.figure()
-    if var == 1:
-        varstr = 'Growth'
-    else:
-        varstr = '%'
     lst1 = []
     lst2 = []
     lst3 = []
@@ -41,7 +72,7 @@ def Graphs(graph1, var):
         lst1.append(elem)
     for elem in graph1['Population']:
         lst2.append(elem)
-    for elem in graph1[varstr]:
+    for elem in graph1['Growth']:
         lst3.append(elem)
     ax1.bar(lst1, lst2, color='green')
     ax2.errorbar(lst1, lst3)
@@ -52,15 +83,18 @@ xls = xls.parse('Sheet1')
 df = pd.DataFrame(xls)
 str1 = df.sort_index(by='Year', ascending=True)  # sort index
 
-str1 = Reindex1(str1)
-strdel = Reindex1(str1)
+str1 = reindex1(str1)
+strdel = reindex1(str1)
 indexstr = 0
 for elem in str1['Year']:  # delete elements < 1953
     if elem < 1953:
         strdel = strdel.drop(indexstr, axis=0)
     indexstr += 1
 
-NewIndexPop(strdel)
-print(strdel)
-var = 1
-Graphs(strdel, var)
+new_index_pop(strdel)
+
+root = tkinter.tix.Tk()
+main_window(root, strdel)
+
+graphs(strdel)
+root.mainloop()
